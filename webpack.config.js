3adcd896c93bf76b56f5f067245d3ba13cb80e5e
Kraig Hanson <KraigGit@GMail.Com>
2020-01-24 16:36:13 -0700

    Completed 'hello world' react/jsx client. Updated ports.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'www'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
