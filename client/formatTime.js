8ddb7a3a31c3461939e87dba2ed11d94a0dfbec4
Kraig Hanson <KraigGit@GMail.Com>
2020-01-28 09:24:02 -0700

    Completed tests for formatTime

--- client/formatTime.js
+++ client/formatTime.js
@@ -1,19 +1,21 @@
 function formatTime(seconds) {
   let str = '';
 
-  const hours = Math.floor(seconds / 3600);
+  const roundedSeconds = Math.round(seconds);
+
+  const hours = Math.floor(roundedSeconds / 3600);
   if (hours) {
     str = `${hours.toString()}:`;
   }
 
-  const minutes = Math.floor((seconds - hours * 3600) / 60);
+  const minutes = Math.floor((roundedSeconds - hours * 3600) / 60);
   if (str !== '') {
     str = `${str}${minutes.toString().padStart(2, '0')}:`;
   } else {
     str = `${minutes.toString()}:`;
   }
 
-  const remainingSeconds = Math.floor(seconds - 3600 * hours - 60 * minutes);
+  const remainingSeconds = Math.floor(roundedSeconds - 3600 * hours - 60 * minutes);
   str = `${str}${remainingSeconds.toString().padStart(2, '0')}`;
 
   return str;
--- tests/Model.test.js
+++ tests/Model.test.js
@@ -1,10 +1,10 @@
-const Model = require('./Model');
+const Model = require('../server/Model');
 const db = require('../db');
 
 beforeAll(() => db.connect());
 
 test('Test: Model.getChatsForVid(1)', () => Model.getChatsForVid(1).then(data => {
-  expect(data.length).toBe(12);
+  expect(data.length).toBe(10);
 }));
 
 afterAll(() => db.disconnect());
