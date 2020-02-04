330dc893062fbed2fdc19a1f34edd1f00745e56c
Kraig Hanson <KraigGit@GMail.Com>
2020-02-04 15:37:22 -0700

    Changed from App to ChatService to support proxying

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,13 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const App = require('./App.jsx');
+const ChatService = require('./ChatService.jsx');
 
 let userId = window.location.search.slice(1);
 
 if (!Number(userId)) userId = 1;
 
 ReactDOM.render(
-  <App userId={userId} />,
-  document.getElementById('app'),
+  <ChatService userId={userId} />,
+  document.getElementById('chat-service'),
 );
